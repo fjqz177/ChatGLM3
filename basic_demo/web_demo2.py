e04bef5de750d28325f0559a067bb8f99817b3da
fjqz177 <fjqz177@139.com>
2023-12-03 21:55:33 +0800

    本地化部署 int4量化

--- basic_demo/web_demo2.py
+++ basic_demo/web_demo2.py
@@ -3,7 +3,7 @@ import streamlit as st
 import torch
 from transformers import AutoModel, AutoTokenizer
 
-MODEL_PATH = os.environ.get('MODEL_PATH', 'THUDM/chatglm3-6b')
+MODEL_PATH = os.environ.get('MODEL_PATH', '.\\model')
 TOKENIZER_PATH = os.environ.get("TOKENIZER_PATH", MODEL_PATH)
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 # 设置页面标题、图标和布局
@@ -17,7 +17,7 @@ st.set_page_config(
 def get_model():
     tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH, trust_remote_code=True)
     if 'cuda' in DEVICE:  # AMD, NVIDIA GPU can use Half Precision
-        model = AutoModel.from_pretrained(MODEL_PATH, trust_remote_code=True).to(DEVICE).eval()
+        model = AutoModel.from_pretrained(MODEL_PATH, trust_remote_code=True).quantize(4).to(DEVICE).eval()
     else:  # CPU, Intel GPU and other GPU can use Float16 Precision Only
         model = AutoModel.from_pretrained(MODEL_PATH, trust_remote_code=True).float().to(DEVICE).eval()
     # 多显卡支持,使用下面两行代替上面一行,将num_gpus改为你实际的显卡数量
